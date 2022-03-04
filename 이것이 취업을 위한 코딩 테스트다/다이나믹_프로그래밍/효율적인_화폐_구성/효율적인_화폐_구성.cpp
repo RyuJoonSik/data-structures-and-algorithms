04f6598c7b26d3167440a408e9090ab5bb4e8133
Ryu Joon Sik <71337000+RyuJoonSik@users.noreply.github.com>
2022-03-04 23:38:13 +0900

    data-structures-algorithms

--- 이것이 취업을 위한 코딩 테스트다/다이나믹_프로그래밍/효율적인_화폐_구성/효율적인_화폐_구성.cpp
+++ 이것이 취업을 위한 코딩 테스트다/다이나믹_프로그래밍/효율적인_화폐_구성/효율적인_화폐_구성.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-  int arr[1001] = {0};
-  int dp[1001] = {1001};
-  dp[0] = 0;
   int n, m, num;
   cin >> n >> m;
-  
-    for (int i = 0; i < m; i++) {
-    // cout << arr[i] << ' ';
-    cout << dp[i] << ' ';
-  }
+  vector<int> arr;
+  vector<int> dp(m + 1, 10001);
+  dp[0] = 0;
+
   for (int i = 0; i < n; i++) {
     cin >> num;
-    arr[i] = num;
+    arr.push_back(num);
   }
 
-  // for (int i = 0; i < n; i++) {
-  //   int count = arr[i];
-  //   // cout << count << ' ';
+  for (int i = 0; i < n; i++) {
+    int count = arr[i];
 
-  //   for (int j = count; j <= m; j++) {
-  //     dp[j] = min(dp[j], dp[j - count] + 1);
-  //     cout << dp[j] << ':' << dp[j - count] + 1 << endl;
-  //   }
-  // }
-  // for (int i = 0; i < n; i++) {
-    // cout << arr[i] << ' ';
-    // cout << dp[i] << ' ';
-  // }
-  // if (dp[m] == 1001) {
-  //   cout << -1;
-  // } else {
-  //   cout << dp[m];
-  // }
+    for (int j = count; j <= m; j++) {
+      dp[j] = min(dp[j], dp[j - count] + 1);
+    }
+  }
+
+  if (dp[m] == 10001) {
+    cout << -1;
+  } else {
+    cout << dp[m];
+  }
 
   return 0;
 }
